c8423b00c537a207812da525a7ba2062ebbdb949
KumarSuPrashant <mrprashant39@gmail.com>
2024-06-05 15:18:10 +0530

    first commit"

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -19,8 +19,7 @@ const Topbar = () => {
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
           <img
-            src="/assets/images/logo.svg"
-            alt="logo"
+            src="/assets/images/pra.PNG"
             width={130}
             height={325}
           />
